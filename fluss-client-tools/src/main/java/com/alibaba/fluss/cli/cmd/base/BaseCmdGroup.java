c41f2b96077f00026647e9ba34b49dd605fa2f67
simonsssu <barley0806@gmail.com>
2025-03-30 22:08:29 +0800

    remove flusscmd

---
+++ fluss-client-tools/src/main/java/com/alibaba/fluss/cli/cmd/base/BaseCmdGroup.java
@@ -0,0 +1,127 @@
+/*
+ *  Copyright (c) 2025 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.alibaba.fluss.cli.cmd.base;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.MissingCommandException;
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.Parameters;
+import com.beust.jcommander.UnixStyleUsageFormatter;
+import org.apache.commons.lang3.StringUtils;
+import org.reflections.Reflections;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public abstract class BaseCmdGroup extends BaseCmd {
+
+    protected String[] args;
+
+    public BaseCmdGroup() {
+        super();
+        initSubCommand();
+    }
+
+    @Override
+    protected String fullNamePrefix() {
+        return "fluss";
+    }
+
+    @Override
+    public void setArgs(String[] args) {
+        this.args = args;
+    }
+
+    @Override
+    public int execute() {
+        try {
+            cmdJc.parse(args);
+            if (cmdJc.getParsedCommand() == null) {
+                System.err.println(
+                        StringUtils.capitalize(getCmdName())
+                                + " Command Error: No sub command specified!");
+                cmdJc.usage();
+                return 1;
+            }
+            BaseCliCmd<?> cmd = getCliCmd(cmdJc.getParsedCommand());
+            if (cmd != null) {
+                return cmd.execute();
+            } else {
+                throw new ParameterException("Unknown command: " + cmdJc.getParsedCommand());
+            }
+        } catch (ParameterException e) {
+            System.err.println(StringUtils.capitalize(getCmdName()) + " Error: " + e.getMessage());
+            if (e instanceof MissingCommandException) {
+                cmdJc.usage();
+            } else {
+                getCliCmd(cmdJc.getParsedCommand()).printUsage();
+            }
+            return 1;
+        }
+    }
+
+    public void initSubCommand() {
+        Reflections reflections = new Reflections("com.alibaba.fluss.cli");
+        List<Class<?>> commands =
+                new ArrayList<>(reflections.getTypesAnnotatedWith(Parameters.class));
+        commands.sort(Comparator.comparing(Class::getName));
+        commands.stream()
+                .filter(
+                        cliClazz -> {
+                            Type type = cliClazz.getGenericSuperclass();
+                            if (type instanceof ParameterizedType) {
+                                ParameterizedType pt = (ParameterizedType) type;
+                                Type[] actualTypeArgs = pt.getActualTypeArguments();
+                                return actualTypeArgs.length > 0
+                                        && actualTypeArgs[0]
+                                                .getTypeName()
+                                                .equals(this.getClass().getName());
+                            } else {
+                                return false;
+                            }
+                        })
+                .forEach(
+                        cmd -> {
+                            try {
+                                BaseCliCmd<?> commandInstance =
+                                        (BaseCliCmd<?>) cmd.getDeclaredConstructor().newInstance();
+                                Arrays.stream(cmd.getAnnotation(Parameters.class).commandNames())
+                                        .forEach(
+                                                commandName -> {
+                                                    cmdJc.addCommand(commandName, commandInstance);
+                                                    JCommander jc =
+                                                            cmdJc.findCommandByAlias(commandName);
+                                                    jc.setUsageFormatter(
+                                                            new UnixStyleUsageFormatter(jc));
+                                                });
+                            } catch (Exception e) {
+                                System.err.println(
+                                        "Initialize sub command failed: " + e.getMessage());
+                                throw new RuntimeException(e);
+                            }
+                        });
+    }
+
+    public BaseCliCmd<?> getCliCmd(String cmdName) {
+        List<Object> subCmds = cmdJc.getCommands().get(cmdName).getObjects();
+        return (BaseCliCmd<?>) subCmds.get(0);
+    }
+}
---
+++ fluss-client-tools/src/main/java/com/alibaba/fluss/cli/cmd/table/CreateTableCmd.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2025 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.alibaba.fluss.cli.cmd.table;
+
+import com.alibaba.fluss.cli.cmd.base.BaseCliCmd;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(commandNames = "create", commandDescription = "Create a new table")
+public class CreateTableCmd extends BaseCliCmd<TableCmdGroup> {
+
+    @Parameter(description = "<table-path>", required = true)
+    private String tablePath;
+
+    @Parameter(names = "--schema", description = "Schema definition file")
+    private String schemaFile;
+
+    @Parameter(names = "--shards", description = "Number of shards")
+    private int shards = 1;
+
+    @Override
+    public int execute() {
+        System.out.printf("Creating table %s with %d shards\n", tablePath, shards);
+        if (schemaFile != null) {
+            System.out.println("Using schema: " + schemaFile);
+        }
+        return 0;
+    }
+}
